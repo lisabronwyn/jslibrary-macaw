e806f3c762c6720833a0427e4216713e7def2739
Lisa Bronwyn <e.bronwyn@gmail.com>
2016-09-27 11:53:51 -0700

    added replace function spec doc and concat doc

---
+++ src/docs/replaceDoc.md
@@ -0,0 +1,22 @@
+#Replace
+
+##Function Description
+
+##replace(string, pattern, replacement)
+
+Looks for pattern in a given string and replaces it with a new string
+###Arguments
+A string
+A pattern to search for in the string
+A string to replace the pattern with
+
+###Return
+
+It returns a string with a replaced value 
+
+Example:
+
+```javascript
+replace('Hello wor world', 'world', 'Lisa')
+//=> 'Hello wor Lisa'
+```
---
+++ src/spec/replaceSpec.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect
+
+import replace from '../lib/replace.js'
+
+describe('replace', () => {
+  it('will look for a pattern in a given string and replace it with something else', () => {
+    expect( replace('Hello wor world', 'world', 'Lisa') ).to.deep.equal('Hello wor Lisa')
+  })
+  it('will return the original string if a pattern is not found', () => {
+    expect( replace('Hello Mike', 'Fred', 'Barney') ).to.deep.equal('Hello Mike')
+  })
+
+})
