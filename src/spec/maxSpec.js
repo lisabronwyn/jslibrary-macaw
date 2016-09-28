25a93f5c7468b48716ec648951fb1b87eeab427a
Devon Wesley <dwesley89@gmail.com>
2016-09-28 10:17:30 -0700

    changed the max function to the bland flow

--- src/spec/maxSpec.js
+++ src/spec/maxSpec.js
@@ -1,21 +1,20 @@
-import max from "../lib/max"
-
 import expect from "expect.js"
+import bland from '../bland'
 
 describe("max", () => {
 	it ("Computes the maximum value of array", () => {
-		expect (max([9,2,3,5])).to.eql(9)
+		expect ( bland.max([9,2,3,5])).to.eql(9)
 	})
 
 	it ("if array is empty, undefined is returned", () => {
-		expect (max([])).to.eql(undefined)
+		expect ( bland.max([])).to.eql(undefined)
 	})
 
 	it ("if array is falsey, undefined is returned", () => {
-		expect (max([null, null])).to.eql(undefined)
+		expect ( bland.max([null, null])).to.eql(undefined)
 	})
 
 	it ("if strings have only numbers, they are treated like numbers", () => {
-		expect (max([3, 2, "4"])).to.eql("4")
+		expect ( bland.max([3, 2, "4"])).to.eql("4")
 	})
 })
