39c3dd2d0e49a67d641993a2470317b596d4026e
Devon Wesley <dwesley89@gmail.com>
2016-09-30 09:01:25 -0700

    reduce takes four arguments

--- src/lib/reduce.js
+++ src/lib/reduce.js
@@ -1,7 +1,7 @@
 export default ( collection, iteratee, accumulator ) => {
   if ( Array.isArray( collection ) || typeof collection === 'object' ) {
     for ( let key in collection ) {
-      accumulator = iteratee( accumulator, collection[ key ], key )
+      accumulator = iteratee( accumulator, collection[ key ], key, collection )
     }
   } else {
     return ""
